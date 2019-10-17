2fc792777d85f5b02f524708dfc4da19dafe4035
kingtarun1 <51032756+kingtarun1@users.noreply.github.com>
2019-10-17 15:17:07 +0530

    Update kang.py

--- stdplugins/kang.py
+++ stdplugins/kang.py
@@ -44,8 +44,8 @@ async def _(event):
 
     me = borg.me
     userid = event.from_id
-    packname = f"@kingtarun1 pack"
-    packshortname = f"TarunMasterpiece{userid}"  # format: Uni_Borg_userid
+    packname = f"@kingtarun1's Masterpiece"
+    packshortname = f"TarunMasterpiece"  # format: Uni_Borg_userid
 
     is_a_s = is_it_animated_sticker(reply_message)
     file_ext_ns_ion = "kingtarun1_Sticker.png"
